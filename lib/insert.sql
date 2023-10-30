24acb68c9872bcccd47ac83732e5704437a7994f
Isaac Song <isaacsongs10@gmail.com>
2023-10-29 21:28:55 -0700

    organizing bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 32, "M", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 21, "M", "Brown", "Crazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 10, "F", "Black", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 50, "M", "Brown", "Nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "M", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 33, "M", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 18, "M", "Black", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 32, "M", "Brown", "Nice", 1);
